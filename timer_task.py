49dddaeb0794b116cb62c0ab75193587e65dcc8d
diixo <anatolii.podoba@gmail.com>
2024-01-02 18:30:31 +0200

    fixed async callback to sync

--- timer_task.py
+++ timer_task.py
@@ -2,7 +2,7 @@ import asyncio
 
 # https://stackoverflow.com/questions/45419723/python-timer-with-asyncio-coroutine
 
-class Timer_Interval:
+class Timer_Task:
     def __init__(self, interval, timer_name, context, callback):
         self._interval = interval
         self._name = timer_name
@@ -15,19 +15,19 @@ class Timer_Interval:
     async def start(self):
         while self._ok:
             await asyncio.sleep(self._interval)
-            await self._callback(self._name, self._context, self)
+            self._callback(self._name, self._context, self)
 
     def cancel(self):
         self._ok = False
         self._task.cancel()
 
 
-async def some_callback_1(timer_name, context, timer):
+def some_callback_1(timer_name, context, timer):
     context['count'] += 1
     print('callback: ' + timer_name + ", count: " + str(context['count']))
 
 
-timer1 = Timer_Interval(interval=1, timer_name="Timer_1", context={'count': 0}, callback=some_callback_1)
+timer1 = Timer_Task(interval=1, timer_name="Timer_1", context={'count': 0}, callback=some_callback_1)
 
 ##########################
 if __name__ == '__main__':
