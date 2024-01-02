1543347a7a6049d8dbaa48af81d363d2c31c127c
diixo <anatolii.podoba@gmail.com>
2024-01-02 04:00:04 +0200

    Separated the create_job(..)

--- main.py
+++ main.py
@@ -6,14 +6,19 @@ class Timer:
     def __init__(self, timeout, callback):
         self._timeout = timeout
         self._callback = callback
+        self._task = None
+
+    def create_job(self):
         self._task = asyncio.ensure_future(self._job())
+        print('Timer::create_job')
 
     async def _job(self):
         await asyncio.sleep(self._timeout)
         await self._callback()
 
     def cancel(self):
-        self._task.cancel()
+        if self._task:
+            self._task.cancel()
 
 async def timeout_callback():
     await asyncio.sleep(0.1)
@@ -22,10 +27,13 @@ async def timeout_callback():
 async def main():
     print('\nfirst example:')
     timer = Timer(2, timeout_callback)  # set timer for two seconds
+    print('timer created')
+    timer.create_job()
     await asyncio.sleep(2.5)            # wait to see timer works
 
     print('\nsecond example:')
     timer = Timer(2, timeout_callback)  # set timer for two seconds
+    timer.create_job()
     await asyncio.sleep(1)
     timer.cancel()                      # cancel it
     await asyncio.sleep(1.5)            # and wait to see it won't call callback
