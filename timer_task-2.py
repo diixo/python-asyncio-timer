fe6cf45a9f6e431d15e38420b445bdde3c6784b7
diixo <anatolii.podoba@gmail.com>
2024-01-04 23:15:25 +0200

    initial commit timer_task-2.py

---
+++ timer_task-2.py
@@ -0,0 +1,47 @@
+import asyncio
+
+# https://stackoverflow.com/questions/45419723/python-timer-with-asyncio-coroutine
+
+class Timer_Task:
+
+    def __init__(self, interval, timer_name, context, callback):
+        self._interval = interval
+        self._name = timer_name
+        self._context = context
+        self._callback = callback
+        self._ok = True
+        self._task = None
+        print(timer_name + " init done")
+
+    async def start(self):
+        while self._ok:
+            await asyncio.sleep(self._interval)
+            self._callback(self._context, self)
+
+    def cancel(self):
+        self._ok = False
+        if self._task:
+            self._task.cancel()
+        print(self._name + "::cancel")
+
+
+def some_callback_1(context, timer):
+    context['count'] += 1
+    print('callback: ' + timer._name + ", count: " + str(context['count']))
+
+###########################################################################
+
+timer1 = Timer_Task(interval=3, timer_name="Timer_1", context={'count': 0}, callback=some_callback_1)
+
+if __name__ == '__main__':
+    
+    loop = asyncio.get_event_loop()
+    timer1._task = loop.create_task(timer1.start())
+
+    try:
+        print('>>>')
+        loop.run_forever()
+        print('<<<')
+    except KeyboardInterrupt:
+        timer1.cancel()
+        print("clean up done")
